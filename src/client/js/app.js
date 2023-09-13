e330d5937215f566a7b042826be7d7203b057f0f
Scadaria 02 <scadaria02@iMac-de-Scadaria-2.local>
2023-09-12 21:09:33 -0300

    weatherbit json error

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -20,9 +20,43 @@ async function geoNamesFun(cityName) {
   }
 }
 
+
+// WeatherBit
+const printWeather = document.getElementById('weather-bit');
+
+async function weatherBitFun(cityName) {
+  const weatherKey = 'bfb73eeaffcb4f1abf2bb159679f0130';
+  const weatherBitAPI = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName},NC&key=${weatherKey}`;
+
+  try {
+    const res = await fetch(weatherBitAPI);
+    const data = await res.json();
+
+    console.log(data)
+
+  } catch (error) {
+    console.log(error)
+  }
+  // fetch(weatherBitAPI)
+  //   .then( res => {
+  //     return res.json()
+  //   })
+  //   .then( data => {
+  //     console.log(data)
+  //   })
+  //   .catch( error => {
+  //     console.log(error)
+  //   })
+}
+
+weatherBitFun();
+
+// Event Listener
 document.getElementById('generate').addEventListener('click', () => {
   const cityName = document.getElementById('zip').value;
   geoNamesFun(cityName);
 });
 
-export { geoNamesFun }
+console.log("teste4")
+
+export { geoNamesFun, weatherBitFun }
